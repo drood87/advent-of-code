b3e64578d2f834539472bae2b09068e571f656af
drood <dbernstein1987@hotmail.com>
2018-12-03 11:01:02 +1100

    added input and script

---
+++ 1/aoc1.js
@@ -0,0 +1,11 @@
+const fetchData = fetch("https://adventofcode.com/2018/day/1/input").then(
+    response => {
+        console.log(fetchData);
+    }
+);
+
+// const newArr = arr.reduce((item, i) => {
+//     item ? + : -
+// },0);
+
+console.log(fetchData);
